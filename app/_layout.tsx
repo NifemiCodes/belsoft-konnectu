0ed1478692649fcae30e9a40579ba32d86076b17
Nifemi <nifemiakingba@gmail.com>
2025-05-09 12:25:18 +0100

    Add project assets. configure svg transformer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    'geist': require('@/assets/fonts/Geist-VariableFont_wght.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return <Stack />;
 }
---
+++ screens/Onboarding.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Onboarding screen</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
+
+const styles = StyleSheet.create({});
